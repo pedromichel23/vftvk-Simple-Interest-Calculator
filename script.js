0221f8f65a153f278d78e9c35ac6e99780b4df06
Pedro Michel <pedromichel23@gmail.com>
2021-07-29 23:31:09 +0000

    finished project

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    //get the values of the inputs
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    //validation to accept only an amount greater than 0.
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("result").innerHTML = "";
+        document.getElementById("principal").focus();
+    }
+    else {
+        //if the amount is greater than 0 then show result
+        document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
+    }
 }
-        
+
+//function for update the input range
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
